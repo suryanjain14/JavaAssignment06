0e8e127ee567468b10952c4bfacffada7373dbc0
Suryan Jain <suryanjain14@gmail.com>
2020-05-06 21:27:33 +0530

    Created by synyan jain

--- src/MyBank.java
+++ src/MyBank.java
@@ -1,28 +1,34 @@
+import org.w3c.dom.ls.LSOutput;
+
 import java.util.Scanner;
+import java.util.SortedMap;
 
 class Account{
     double balance;
-    String name;
     Account(){
         balance=100;
     }
 
-    synchronized void withdraw(double amount){
+    synchronized double withdraw(double amount){
         if((balance-amount)<0){
             System.out.println("Insufficient Balance");
+            return 0;
         }
         else{
             balance=balance-amount;
             System.out.println("transaction successful");
-            System.out.println(balance+" left");
+            System.out.println(balance+"  left");
+            return balance;
         }
 
     }
 
-    synchronized void deposite(double amount){
+    synchronized double deposite(double amount){
         balance=balance+amount;
         System.out.println("transaction successful");
-        System.out.println( "Available balance"+amount );
+        System.out.println( "Available balance  "+balance );
+        return balance;
+
     }
 
 }
@@ -30,12 +36,12 @@ class Account{
 class Deposite implements Runnable{
     Account ac;
     Thread t;
-    private double amount;
+    double amount;
     Deposite(Account ac,double amount){
         this.ac=ac;
-        t=new Thread();
+        t=new Thread(this);
+        this.amount=amount;
         t.start();
-        amount=amount;
     }
 
     @Override
@@ -46,12 +52,14 @@ class Deposite implements Runnable{
 class Withdraw implements Runnable{
     Account ac;
     Thread t;
-    private double amount;
+    Account a=new Account();
+    double amount;
     Withdraw(Account ac,double amount){
         this.ac=ac;
-        this.t= new Thread();
-        this.t.start();
         this.amount=amount;
+        this.t= new Thread(this);
+        t.start();
+
     }
 
     @Override
@@ -64,10 +72,13 @@ public class MyBank {
 
     public static void main(String[] args) {
         Account ac[]= new Account[5];
+
+
         int accindex=0;
         Scanner sc = new Scanner(System.in);
         System.out.println("Welcome to the bank Here you can open upto 5 acc");
         System.out.println("under our new Customer Scheme you will get 100$ on every new account");
+        int cc=0;
         while (true){
 
             System.out.println("press 1 for new acc ,2 if you already have an account,3 to exit");
@@ -77,13 +88,79 @@ public class MyBank {
                     if(accindex<4){
                         ac[accindex]=new Account();
                         System.out.println("Your Acc no is "+accindex+"Remember it");
+                        accindex++;
+                        System.out.println("processing Automatic login request");
+                        cc=1;
                     }
                     else{
                         System.out.println("Sorry you cannot open a  account");
                     }
 
-
                 }
+                case 2: {
+                    int acc;
+                    if(cc==0) {
+                        System.out.println("Enter Your Account");
+                        acc = sc.nextInt();
+                    }
+                    else{
+                        acc=accindex-1;
+                    }
+
+                    if (acc>=accindex)
+                    {
+                        System.out.println("wrong entry");
+                        break;
+                    }
+                    boolean loggedin=true;
+                    while (loggedin)
+                    {
+                        System.out.println("wlecome \"" +acc+"\"");
+                        System.out.println("press 1 to deposite ,2 to withdraw,3 to logout");
+                        int j = sc.nextInt();
+                        double amount;
+                        switch (j){
+
+                            case 1:{
+                                System.out.println("Enter amount");
+                                amount=sc.nextDouble();
+                                new Deposite(ac[acc],amount);
+                                try {
+                                    Thread.sleep(100);
+                                } catch (InterruptedException e) {
+                                    e.printStackTrace();
+                                }
+
+                            }
+                            case 2:{
+                                System.out.println("Enter amount to withdraw");
+                                amount=sc.nextDouble();
+                                new Withdraw(ac[acc],amount);
+                                try {
+                                    Thread.sleep(100);
+                                } catch (InterruptedException e) {
+                                    e.printStackTrace();
+                                }
+                                break;
+                            }
+                            case 3:
+                            {
+                                loggedin=false;
+                                break;
+                            }
+
+                        }
+
+
+                    }
+                 break;
+                }
+
+                case 3:{
+                    System.out.println("Bye Bye");
+                    System.exit(1);
+                }
+
             }
 
 
