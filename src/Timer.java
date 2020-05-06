be9c82e8a3c0ead8e0a6193eb1dcb650d8d73ddd
Suryan Jain <suryanjain14@gmail.com>
2020-05-06 12:25:18 +0530

    b

--- src/Timer.java
+++ src/Timer.java
@@ -1,2 +1,28 @@
 public class Timer {
+    public static void main(String[] args) {
+        System.out.println("Welcome to 1000 sec timer");
+        ClockWork c=new ClockWork("TimerThread");
+    }
+}
+
+class ClockWork implements Runnable {
+    Thread t;
+
+    ClockWork(String name) {
+        t = new Thread(this, name);
+        t.start();
+    }
+
+    @Override
+    public void run() {
+        for (int i = 1; i < 1000; i++) {
+            System.out.println("Time left :" + (1000 - i)+" sec");
+            try {
+                t.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
 }
