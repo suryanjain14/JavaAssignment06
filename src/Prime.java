e931624689af881f5b16c00e620f759dadfe00b7
Suryan Jain <suryanjain14@gmail.com>
2020-05-06 10:47:21 +0530

    Initial commit

---
+++ src/Prime.java
@@ -0,0 +1,18 @@
+import java.lang.Math;
+
+public class Prime implements Runnable{
+    double no;
+    Prime(double no){
+        this.no=no;
+    }
+    @Override
+    public void run() {
+        for(int i=0,i<Math.sqrt(no),i++){
+            
+        }
+
+
+    }
+
+
+}
